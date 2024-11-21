9bad11365c4d73ce496def716e385bf096b1029b
RizzoKa <106705500+rizzoka@users.noreply.github.com>
2024-11-21 19:31:03 -0300

    Update Penteado.java

--- projetoSemestral/src/entidades/Penteado.java
+++ projetoSemestral/src/entidades/Penteado.java
@@ -1,5 +1,30 @@
 package entidades;
 
-public class Penteado extends ServicoAbstrata {
+import entidades.Enumeradas.Penteados;
 
+public class Penteado extends ServicoAbstrata {
+	
+	private Penteados penteado;
+
+	public Penteado(char curvatura, char comprimento, Penteados penteado) {
+		super(curvatura, comprimento);
+		this.penteado = penteado;
+	}
+
+	public Penteados getPenteado() {
+		return penteado;
+	}
+
+	public void setPenteado(Penteados penteado) {
+		this.penteado = penteado;
+	}
+	
+	@Override
+	public Double valor() {
+		if (penteado == Penteados.COQUE) {
+			return 15.0;
+		} else {
+			return 30.0;
+		}
+	}
 }
