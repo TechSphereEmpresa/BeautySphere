8dbc14771f85d4517a02d86ae65fa518e20b2aaa
TechSphere <techsphereempresa@gmail.com>
2024-11-23 22:20:21 +0000

    correções

--- projetoSemestral/src/aplicacao/Programa.java
+++ projetoSemestral/src/aplicacao/Programa.java
@@ -129,141 +129,7 @@ public class Programa {
 		System.out.println("Cronograma da semana:");
 		agenda.exibirAgenda();
 
-		// Cartão ou Dinheiro
-		package aplicacao;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
-import entidades.Agenda;
-import entidades.Caixa;
-import entidades.Cliente;
-import entidades.Corte;
-import entidades.Penteado;
-import entidades.Servico;
-import entidades.Enumeradas.Cortes;
-import entidades.Enumeradas.Dias;
-import entidades.Enumeradas.Horas;
-import entidades.Enumeradas.Penteados;
-import entidades.Cartao;
-import entidades.Dinheiro;
-import entidades.Pagamento;
-
-public class Programa {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		List<Cliente> listaClientes = new ArrayList<>();
-		List<Horas> horariosDisponiveis = new ArrayList<>();
-		Agenda agenda = new Agenda(listaClientes, horariosDisponiveis);
-		Servico servico;
-
-		System.out.println("Registro de clientes da TechSphere");
-		System.out.println();
-		System.out.println("Olá, BeautySphere!");
-		System.out.println();
-		System.out.println("Quantos clientes para essa semana?");
-		int n = sc.nextInt();
-		sc.nextLine();
-
-		// Digitar clientes
-		for (int i = 0; i < n; i++) {
-			System.out.println();
-			System.out.println("Cliente #" + (i + 1) + ":");
-			System.out.println("Nome:");
-			sc.next();
-			String nome = sc.nextLine();
-
-			// Escolher horario
-			System.out.println("Horários disponíveis:");
-			agenda.exibirHorarios();
-
-			System.out.println("Escolha um horário:");
-			String horarioEscolhido = sc.nextLine().toUpperCase();
-			agenda.marcarHorario(horarioEscolhido);
-
-			// Digitar curvatura
-			System.out.println();
-			System.out.println("Liso (1), Ondulado (2), Cacheado (3), Crespo (4)");
-			System.out.println("Qual a curvatura do cabelo?");
-			char curvaturaChar = sc.next().charAt(0);
-
-			String curvatura = null;
-			if (curvaturaChar == '1') {
-				curvatura = "liso";
-			} else if (curvaturaChar == '2') {
-				curvatura = "ondulado";
-			} else if (curvaturaChar == '3') {
-				curvatura = "cacheado";
-			} else if (curvaturaChar =='4') {
-				curvatura = "crespo";
-			}
-
-			// Digitar comprimento
-			System.out.println();
-			System.out.println("Curto (1), Médio (2), Longo (3)");
-			System.out.println("Qual o comprimento do cabelo?");
-			char comprimentoChar = sc.next().charAt(0);
-
-			String comprimento = null;
-			if (comprimentoChar == '1') {
-				comprimento = "curto";
-			} else if (comprimentoChar == '2') {
-				comprimento = "médio";
-			} else if (comprimentoChar == '3') {
-				comprimento = "longo";
-			}
-
-			// Escolher servico
-			System.out.println();
-			System.out.println("Escolha o serviço: corte (C) ou penteado (P):");
-			char servicoEscolhido = sc.next().toUpperCase().charAt(0);
-
-			// Escolher corte
-			if (servicoEscolhido == 'C') {
-
-				System.out.println();
-				System.out.println("Cortes disponíveis:");
-				for (Cortes corte : Cortes.values()) {
-					System.out.println("• " + corte);
-				}
-
-				System.out.println();
-				System.out.println("Escolha o corte:");
-				Cortes corte = Cortes.valueOf(sc.next().toUpperCase());
-
-				servico = new Corte(curvatura, comprimento, corte);
-				Cliente cliente = new Cliente(nome, horarioEscolhido, servico);
-				agenda.addCliente(cliente);
-
-				// Escolher penteado
-			} else if (servicoEscolhido == 'P') {
-
-				System.out.println();
-				System.out.println("Penteados disponíveis:");
-				for (Penteados penteado : Penteados.values()) {
-					System.out.println("• " + penteado);
-				}
-
-				System.out.println();
-				System.out.println("Escolha o penteado:");
-				Penteados penteado = Penteados.valueOf(sc.next().toUpperCase());
-
-				servico = new Penteado(curvatura, comprimento, penteado);
-				Cliente cliente = new Cliente(nome, horarioEscolhido, servico);
-				agenda.addCliente(cliente);
-				
-			} else {
-				System.out.println("Serviço inválido");
-			}
-		}
-
-		// Exibir a agenda
-		System.out.println();
-		System.out.println("Cronograma da semana:");
-		agenda.exibirAgenda();
-
-		// Cartão ou Dinheiro
+		/*// Cartão ou Dinheiro
 
 		System.out.println();
 		System.out.println("Escolha a forma de pagamento: Cartão (C) ou Dinheiro (D):");
@@ -286,7 +152,7 @@ public class Programa {
 			}
 		} else {
 			System.out.println("Forma de pagamento inválida.");
-		}
+		}*/
 	}
 }
 // Colocar parte da fatura total
--- projetoSemestral/src/entidades/Enumeradas/Horas.java
+++ projetoSemestral/src/entidades/Enumeradas/Horas.java
@@ -1,27 +1,23 @@
 package entidades.Enumeradas;
 
 public enum Horas {
-	H10, H11, H13, H14, H15, H16, H17;
-	
-	@Override
-    public String toString() {
-        switch (this) {
-            case H10:
-                return "10:00 (H10)";
-            case H11:
-                return "11:00 (H11)";
-            case H13:
-            	return "13:00 (H13)";
-            case H14:
-            	return "14:00 (H14)";
-            case H15:
-            	return "15:00 (H15)";
-            case H16:
-            	return "16:00 (H16)";
-            case H17:
-            	return "17:00 (H17)";
-            default:
-                return super.toString();
-        }
-    }
+	H10("10:00"), 
+	H11("11:00"), 
+	H13("13:00"),
+	H14("14:00"),
+	H15("13:00"),
+	H16("16:00"),
+	H17("17:00");
+
+	private final String hora;
+
+	// Associar o valor
+	Horas(String hora) {
+	        this.hora = hora;
+	    }
+
+	// Obter o valor associado
+	public String getHora() {
+		return hora;
+	}
 }
